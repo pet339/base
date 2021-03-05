1d7493f1cfc38f980d3dd6475854ab1359319e5a
pet339 <petra.pristyak@gmail.com>
2021-03-05 19:36:39 +0100

    tachograph test added

---
+++ train-tachograph/src/test/java/hu/bme/mit/train/tachograph/TachographTest.java
@@ -0,0 +1,19 @@
+package hu.bme.mit.train.tachograph;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TachographTest {
+    TrainTachographImpl t;
+
+    @Before
+    public void before() {
+        t = new TrainTachographImpl("1", "1", "1");
+    }
+
+    @Test
+    public void CollectionHasSomeElements() {
+        Assert.assertEquals(false, t.tachographTable.isEmpty());
+    }
+}
