4c0a7a51ee813e90a4ae5c6bdfcb7f9fcfea959c
pet339 <petra.pristyak@gmail.com>
2021-03-05 20:15:05 +0100

    tachograph test

--- train-tachograph/src/main/java/hu/bme/mit/train/tachograph/TrainTachographImpl.java
+++ train-tachograph/src/main/java/hu/bme/mit/train/tachograph/TrainTachographImpl.java
@@ -6,6 +6,7 @@ import com.google.common.collect.Table;
 public class TrainTachographImpl {
     Table<String, String, Integer> tachographTable =  HashBasedTable.create();
 
+
     public TrainTachographImpl(String currentTime, String joystickPosition, Integer referenceSpeed) {
         tachographTable.put(currentTime, joystickPosition, referenceSpeed);
     }
